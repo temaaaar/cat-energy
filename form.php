92c8609fbb7aa0d32ebed987ad209352b668a18a
Timur Kudanov <136966277+temaaaar@users.noreply.github.com>
2024-10-12 12:37:57 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
-<?
+<?php
 
-/*получаем значения полей из формы*/
+/* Получаем значения полей из формы */
 $name = $_POST['name'];
 $weight = $_POST['weight'];
 $old = $_POST['old'];
@@ -15,30 +15,32 @@ $water = $_POST['water'];
 $milk = $_POST['milk'];
 $vitamins = $_POST['vitamins'];
 
+/* Формируем сообщение */
+$message = "Имя: $name\nВес: $weight\nВозраст: $old\nПотеря веса: $weight_loss\nПрибавка веса: $weight_gain\nСовет: $advice\nEmail: $email\nТелефон: $phone\nКомментарий: $comment\nСахарозаменитель: $sweetener\nВода: $water\nМолоко: $milk\nВитамины: $vitamins";
 
-/*функция для создания запроса на сервер Telegram */
-function parser($url){
+/* Функция для создания запроса на сервер Telegram */
+function parser($url) {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     $result = curl_exec($curl);
-    if ($result == false) {     
-      echo "Ошибка отправки запроса: " . curl_error($curl);
-      return false;
+    if ($result === false) {
+        echo "Ошибка отправки запроса: " . curl_error($curl);
+        return false;
     }
-    else {
-      return true;
-    }
+    return true;
 }
 
 function orderSendTelegram($message) {
-
-    /*токен который выдаётся при регистрации бота */
+    /* Токен, который выдаётся при регистрации бота */
     $token = "7633312262:AAEkc4NguLm3cUy3sbaVs8iFl13eCDeUccs";
-    /*идентификатор группы*/
+    /* Идентификатор группы */
     $chat_id = "-4519183749";
 
-    /*делаем запрос*/
-    parser("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$message}");
-
+    /* Делаем запрос */
+    parser("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text=" . urlencode($message));
 }
+
+/* Отправляем сообщение в Telegram */
+orderSendTelegram($message);
+?>
